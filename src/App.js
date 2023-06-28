fe0fb377dccb32f4756c7f6fd5330e99aaa6b37f
agtTwilight <lukasmacmillen@gmail.com>
2023-06-28 13:17:04 -0700

    firebase user auth init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import 'fireabase/auth';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { Home } from './pages/Home';
+import { Profile } from './pages/Profile';
+
+firebase.initializeApp({
+	apiKey: 'AIzaSyAJ3utzwJu2hKZRost8GK2XcYFTU5z1_bI',
+	authDomain: 'nfc-app-7f535.firebaseapp.com',
+	projectId: 'nfc-app-7f535',
+	storageBucket: 'nfc-app-7f535.appspot.com',
+	messagingSenderId: '1053572299085',
+	appId: '1:1053572299085:web:12206087f4e894019c5f04',
+	measurementId: 'G-XGE7TLKVFX',
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Check if user is signed in with google
+	// logged in -> user = true. Else, user = null.
+	const [user] = useAuthState(auth);
+
+	return (
+		<div className="App">
+			<header></header>
+			<section>
+				{/* Check if user is logged in. Use terneray operator to show correct page */}
+				{user ? <Profile /> : <Home />}
+			</section>
+		</div>
+	);
 }
 
 export default App;
